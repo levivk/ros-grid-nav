842fac633cb5a136baa1ad888fb63e0ecdb0ac39
Levi Vande Kamp <levi.vandekamp@gmail.com>
2022-05-01 22:51:43 -0500

    gitignore and deleted things

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
-print("In setup.py")
-d = generate_distutils_setup(
-    packages=['grid_nav'],
-    package_dir={'': 'src'}
-)
-setup(**d)
--- src/grid_nav/locator.py
+++
@@ -1,7 +0,0 @@
-import rospy
-
-class grid_locate:
-
-    def __init__(self):
-
-        self._loc_sub = rospy.Subscriber
